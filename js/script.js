5a21eb1c5dc15a43178ab4df519ecf493546efca
Yessenali Yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-01-09 16:20:46 +0600

    task 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 "use strict";
-
-
 //1
 const numberOfFilms = +prompt("How many films have you seen?", "");
 //2
@@ -12,7 +10,31 @@ const personalMovieDB = {
     privat:false,
 };
 //3
-let movie = prompt("Your last seen film?","");
-let rating = prompt("Your rating to that movie?", "");
+let movie, rating;
+for(let i =0;i<2;i++){
+    movie = prompt("Your last seen film?");
+    rating = prompt("Your rating to that movie?");
+    if(movie != null && rating != null && movie != '' && rating !='' && movie.length<50){
+        personalMovieDB.movies[movie]=rating;
+        console.log('done');
+    }else{
+        console.log('error');
+        i--;
+    }
 
-personalMovieDB.movies[movie]=rating;
+}
+
+if(personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов");
+}else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
+    console.log("Вы классический зритель");
+}else if(personalMovieDB.count>=30){
+    console.log("Вы киноман");
+}else{
+    console.log("Произошла ошибка");
+}
+
+
+
+
+console.log(personalMovieDB);
