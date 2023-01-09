f990722cb56c738d1f3d32dc02e91f8314c2f3a8
Yessenali Yerkebulan <yerkebulan.yessenali@narxoz.kz>
2023-01-09 10:50:45 +0600

    thirs commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 "use strict";
 
-for(let i=1;i<=7;i++){
-    for(let j=0;j<i;j++){
-        console.log('#');
-    }
-}
+
+//1
+const numberOfFilms = +prompt("How many films have you seen?", "");
+//2
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false,
+};
+//3
+let movie = prompt("Your last seen film?","");
+let rating = prompt("Your rating to that movie!", "");
+
+const movies = {
+    movie: rating,
+};
